ce21434bec3ef835a0c4f434d85842df385c9cea
Aristide <asoullard@aso-PC.interdata-ad.int>
2014-11-13 17:01:07 +0100

    fix insight reco

--- J3telQueryBundle.php
+++ J3telQueryBundle.php
@@ -2,7 +2,6 @@
 namespace J3tel\QueryBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class J3telQueryBundle extends Bundle
 {
